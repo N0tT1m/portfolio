c7d7e5a4c5ea45e3b07bad0bc67abb18311a623b
Timmy <nathan.moritz@duocore.dev>
2024-03-26 02:58:23 -0400

    Added users andd groups

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = [
     "192.168.1.89"
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
